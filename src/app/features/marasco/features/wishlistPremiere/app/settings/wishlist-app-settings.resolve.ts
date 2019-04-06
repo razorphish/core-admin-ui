8d53df7c212ea204e0bf963beea78ce7d18e743c
razorphish <david@maras.co>
2019-04-06 08:36:00 -0700

    removed unused lines of code

--- src/app/features/marasco/features/wishlistPremiere/app/settings/wishlist-app-settings.resolve.ts
+++ src/app/features/marasco/features/wishlistPremiere/app/settings/wishlist-app-settings.resolve.ts
@@ -1,19 +1,15 @@
 
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { of } from 'rxjs';
 
 // Local
 import { WishlistAppService } from '../shared/wishlist-app-service';
 
-import { ActivityLogSubjectService } from '../../../../shared/activitylog.subject-service';
-
 @Injectable()
 export class WishlistAppSettingsResolve implements Resolve<any> {
   constructor(
-    private _wishlistAppSettingsService: WishlistAppService,
-    private _activityLogService: ActivityLogSubjectService
-  ) {}
+    private _wishlistAppSettingsService: WishlistAppService
+  ) { }
 
   resolve(route: ActivatedRouteSnapshot) {
     return this._wishlistAppSettingsService.all();
